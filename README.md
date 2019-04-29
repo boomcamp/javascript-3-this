2202602fa9f00bfcce3ef6d8378a43349be3c6b5
Aodhan Hayter <aodhan.hayter@gmail.com>
2019-04-29 15:13:13 -0600

    Initial

---
+++ README.md
@@ -0,0 +1,20 @@
+# Project Summary
+---
+Learn how to utilize the `this` keyword in JavaScript through a series of practical exercises.
+
+## Setup
+
+* `Fork` this repository.
+* `Clone` your fork.
+* Open `./practice.js` with your code editor.
+* Open `./index.html` with your browser.
+
+## Directions
+
+Try and complete all of the activities in `./practice.js` to make the spec runner pass it's tests.
+In order to check the progress of the spec runner, open `./SpecRunner.html` with your browser.
+Remember to commit and push your code often. Good luck!
+
+## Resources
+
+[MDN this](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/this)
---
+++ practice.js
@@ -0,0 +1,84 @@
+//We're in a job interview. Answer the following questions (try to not look at your notes unless you have to).
+  // 1) What is the purpose of the 'this keyword'?
+
+      //Answer
+
+  // 2) What are the four rules that govern what the 'this keyword' is bound to and describe each?
+
+      //Answer
+
+  // 3) What does .bind do?
+
+      //Answer
+
+
+//Next Problem
+
+//Create an object called user which has the following properties.
+  //username --> which is a string
+  //email --> which is a string
+  //getUsername --> which is a function that returns the current object's username property. *Don't use 'user' instead use the 'this' keyword*
+
+    //Code Here
+
+//Now, invoke the getUsername method and verify you got the username of the object and not anything else.
+
+
+//Next Problem
+
+
+// Write a constructor function, including method definitions, which will make the following function invocations function properly.
+
+  //Function Invocations Here
+
+var prius = new Car('Toyota', 'Prius', 2011);
+var mustang = new Car('Ford', 'Mustang', 2013);
+
+//Hint, you'll need to add a move property, with a starting value of zero, and write a moveCar function which will increment the move property by 10. The move property will be added to every object that is being returned from the Car function. You'll also need to use the 'this' keyword properly in order to make sure you're invoking moveCar on the right object (prius vs mustang).
+
+prius.moveCar(); //increments prius' move property by 10. Returns the new move property.
+mustang.moveCar(); //increments mustang' move property by 10. Returns the new move property.
+
+
+
+//Continuation of previous problem
+
+var getYear = function(){
+  return this.year;
+};
+
+// Above you are given a getYear function.  Use the bind function to bind the prius object to the function and save it in a variable called getPriusYear
+// Then use the bind function to bind the mustang to the function and save it in a varabile called getMustangYear
+// Console Log the results of the getPriusYear and getMustangYear to see that they are returning the correct years.
+
+
+//Note(no tests)
+  //Code Here
+
+
+
+//New Problem
+
+var myUser = {
+ username: 'iliketurtles',
+ age: 13,
+ email: 'iliketurtles@gmail.com'
+};
+
+var getMyUsername = function() {
+ return this.username;
+};
+
+var userName = getMyUsername(); //Fix this
+
+//Above you're given an object, and  a function. What will the getMyUsername function return?
+//Note(no tests)
+  //Answer Here
+
+//In the example above, what is the 'this keyword' bound to when getMyUsername runs?
+
+  //Answer Here
+
+
+//Fix the getMyUsername invocation (stored in the userName variable, at the bottom of the above code) so that userName will be equal to 'iliketurtles'.
+
