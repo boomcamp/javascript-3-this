84d026ce8d290b3297470a36370504383de2a067
Robby Ann Eslava <50604868+rbbynnslv@users.noreply.github.com>
2019-05-27 14:55:45 +0800

    Update practice.js

--- practice.js
+++ practice.js
@@ -48,15 +48,17 @@ function Car(make, model, year, move) {
   this.make = make;
   this.model = model;
   this.year = year;
-  this.move = move;
+  this.move = 0;
+
+  this.moveCar = function() {
+    return this.move += 10
+  }
 }
 
 var prius = new Car('Toyota', 'Prius', 2011, 0);
 var mustang = new Car('Ford', 'Mustang', 2013, 0);
 
 //Hint, you'll need to add a move property, with a starting value of zero, and write a moveCar function which will increment the move property by 10. The move property will be added to every object that is being returned from the Car function. You'll also need to use the 'this' keyword properly in order to make sure you're invoking moveCar on the right object (prius vs mustang).
-
-
 
 prius.moveCar(); //increments prius' move property by 10. Returns the new move property.
 mustang.moveCar(); //increments mustang' move property by 10. Returns the new move property.
@@ -75,8 +77,6 @@ var getYear = function(){
 
 
 //Note(no tests)
-
-
 
 
 
