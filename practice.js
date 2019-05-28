37ecd45892f2d24701ed15f39031ef58a545a34b
Noel Layante <noel.layante@boom.camp>
2019-05-28 13:51:35 +0800

    Done Problem 4

--- practice.js
+++ practice.js
@@ -90,16 +90,17 @@ var getMyUsername = function() {
  return this.username;
 };
 
-var userName = getMyUsername(); //Fix this
+var userName = getMyUsername.bind(myUser)(); //Fix this
 
 //Above you're given an object, and  a function. What will the getMyUsername function return?
 //Note(no tests)
   //Answer Here
+  'the value of the username property of myUser, which is iliketurtles';
 
 //In the example above, what is the 'this keyword' bound to when getMyUsername runs?
 
   //Answer Here
-
+  'myUser';
 
 //Fix the getMyUsername invocation (stored in the userName variable, at the bottom of the above code) so that userName will be equal to 'iliketurtles'.
 
