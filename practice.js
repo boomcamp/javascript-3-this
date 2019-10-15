98a00c1b9c3f9fb34b86b94d1374efbc6a0807b4
ginoAquino7 <gino.aquino@boom.camp>
2019-10-15 17:41:02 +0800

    submission

--- practice.js
+++ practice.js
@@ -1,15 +1,24 @@
 //We're in a job interview. Answer the following questions (try to not look at your notes unless you have to).
   // 1) What is the purpose of the 'this keyword'?
-
+      
       //Answer
+
+      // * this keyword can refers to the owner object in a method and global object when in function.
 
   // 2) What are the four rules that govern what the 'this keyword' is bound to and describe each?
 
       //Answer
 
-  // 3) What does .bind do?
+      //* 1.If the 'new' keyword is used when calling the function 
+      //* 2.If 'apply','call', or 'bind' are used to call a function
+      //* 3.If the function is called as a method
+      //* 4.If a function is invoked as a free function invocation
 
+  // 3) What does .bind do?
+     
       //Answer
+
+       // * can allow us to set the 'this' values but does not actually call the function.
 
 
 //Next Problem
@@ -19,10 +28,18 @@
   //email --> which is a string
   //getUsername --> which is a function that returns the current object's username property. *Don't use 'user' instead use the 'this' keyword*
 
+  const user = {
+    username : 'ginoAquino7',
+    email : 'gino.aquino@boom.camp',
+    getUsername(){
+      return this.username;
+    }
+  };
     //Code Here
-
+    
 //Now, invoke the getUsername method and verify you got the username of the object and not anything else.
 
+user.getUsername();
 
 //Next Problem
 
@@ -31,10 +48,19 @@
 
   //Function Invocations Here
 
+function  Car(brand,model,year){
+  this.brand = brand;
+  this.model = model;
+  this.year = year;
+}
+
 var prius = new Car('Toyota', 'Prius', 2011);
 var mustang = new Car('Ford', 'Mustang', 2013);
 
-//Hint, you'll need to add a move property, with a starting value of zero, and write a moveCar function which will increment the move property by 10. The move property will be added to every object that is being returned from the Car function. You'll also need to use the 'this' keyword properly in order to make sure you're invoking moveCar on the right object (prius vs mustang).
+//Hint, you'll need to add a move property, with a starting value of zero, and write a moveCar function which will increment the move property by 10. 
+//The move property will be added to every object that is being returned from the Car function. You'll also need to use the 'this' keyword properly in 
+//order to make sure you're invoking moveCar on the right object (prius vs mustang).
+
 
 prius.moveCar(); //increments prius' move property by 10. Returns the new move property.
 mustang.moveCar(); //increments mustang' move property by 10. Returns the new move property.
@@ -47,6 +73,7 @@ var getYear = function(){
   return this.year;
 };
 
+
 // Above you are given a getYear function.  Use the bind function to bind the prius object to the function and save it in a variable called getPriusYear
 // Then use the bind function to bind the mustang to the function and save it in a varabile called getMustangYear
 // Console Log the results of the getPriusYear and getMustangYear to see that they are returning the correct years.
@@ -54,31 +81,41 @@ var getYear = function(){
 
 //Note(no tests)
   //Code Here
-
+  
+  function Car(brand, model ,year, move){
+     this.brand = brand;
+     this.model = model;
+     this.year = year;
+     this.move  = 0;
+     this.moveCar = function() {
+      return this.move += 10
+    }
+  }
 
 
 //New Problem
 
 var myUser = {
- username: 'iliketurtles',
- age: 13,
- email: 'iliketurtles@gmail.com'
-};
-
-var getMyUsername = function() {
- return this.username;
-};
-
-var userName = getMyUsername(); //Fix this
+  username: 'iliketurtles',
+  age: 13,
+  email: 'iliketurtles@gmail.com',
+   getMyUsername() {
+  return this.username;
+ }
+ };
+ myUser.getMyUsername(); //Fix this
 
 //Above you're given an object, and  a function. What will the getMyUsername function return?
 //Note(no tests)
   //Answer Here
 
+  //
+
 //In the example above, what is the 'this keyword' bound to when getMyUsername runs?
 
   //Answer Here
-
+ 
+  // 
 
 //Fix the getMyUsername invocation (stored in the userName variable, at the bottom of the above code) so that userName will be equal to 'iliketurtles'.
 
